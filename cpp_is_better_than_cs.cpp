7e88b97d04a8ec498e2e5fbb7ec4a7238d90a90c
EricWang559 <112424879+EricWang3@users.noreply.github.com>
2024-10-03 15:07:53 -0500

    udders

---
+++ cpp_is_better_than_cs.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+
+    cin >> vector<int> vect;
+
+    for(int i = 0; i < vect.length(); i++){
+        cout << vect[i] << " " << flush;
+    }
+}
